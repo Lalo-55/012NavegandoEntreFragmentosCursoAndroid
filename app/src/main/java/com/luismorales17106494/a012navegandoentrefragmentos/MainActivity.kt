56dac631f34bf54427a76f6815471596d5fc502f
Edu-Lu <moralesluis17106494@gmail.com>
2022-08-02 14:09:02 -0500

    Clase 012 Navegando entre Fragmentos clase Completada

--- app/src/main/java/com/luismorales17106494/a012navegandoentrefragmentos/MainActivity.kt
+++ app/src/main/java/com/luismorales17106494/a012navegandoentrefragmentos/MainActivity.kt
@@ -2,10 +2,24 @@ package com.luismorales17106494.a012navegandoentrefragmentos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*//Acceder a un fragmento desde el main activity
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragment_container_view, PrimerFragment())
+        }
+
+        //Desde el xml, se pone en el fragment container
+        //android:name="com.luismorales17106494.a012navegandoentrefragmentos.PrimerFragment"
+
+         */
+
+
     }
 }
---
+++ app/src/main/java/com/luismorales17106494/a012navegandoentrefragmentos/PrimerFragment.kt
@@ -0,0 +1,31 @@
+package com.luismorales17106494.a012navegandoentrefragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.commit
+
+class PrimerFragment : Fragment(R.layout.fragment_primer) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val boton = view.findViewById<Button>(R.id.btnNavegar)
+        boton.setOnClickListener {
+            requireActivity().supportFragmentManager.commit {
+                //Agrega se encima sobre el fragment
+                // add(R.id.fragment_container_view, SegundoFragment())
+
+                //Remplaza el fragmento
+                replace(R.id.fragment_container_view, SegundoFragment())
+                addToBackStack("primerFragment")
+            }
+        }
+
+    }
+
+
+}
